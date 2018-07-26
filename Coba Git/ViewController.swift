13a1d679f1f475ff58e1e7b5a22725922c7ce2fc
Natalia Burhan <nattokon@gmail.com>
2018-07-26 09:58:24 +0700

    Initial Commit

---
+++ Coba Git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Coba Git
+//
+//  Created by Natalia Burhan on 26/07/18.
+//  Copyright © 2018 Natalia Burhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
